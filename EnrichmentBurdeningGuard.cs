fbe57cb633b3dd569ebf6a952c484737941f3a6e
Pierce Thompson <pthomp131@gmail.com>
2025-09-10 12:03:31 -0400

    first passes on the gravity enrichments

---
+++ EnrichmentBurdeningGuard.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using ThunderRoad;
+using ThunderRoad.Skill.Spell;
+using UnityEngine;
+
+namespace Enrichments;
+
+public class EnrichmentBurdeningGuard : EnrichmentData
+{
+    public string effectId;
+    public float forceMultiplier = 15f;
+    public float requiredVelocity = 0.5f;
+    public float creatureCooldown = 2f;
+    
+    public EffectData effectData;
+
+    private List<Creature> seenCreatures = new();
+    
+    public bool IsValidTarget(Creature target, SpellCastCharge spellData) => target != null && !target.isCulled && target != spellData?.spellCaster.ragdollHand.creature && !seenCreatures.Contains(target);
+
+    public override void OnCatalogRefresh()
+    {
+        base.OnCatalogRefresh();
+        effectData = Catalog.GetData<EffectData>(effectId);
+    }
+
+    public override void OnItemImbued(Item item, Imbue imbue, SpellCastCharge spellCastCharge)
+    {
+        base.OnItemImbued(item, imbue, spellCastCharge);
+        if (spellCastCharge is not SpellCastGravity) return;
+        EventManager.onCreatureAttackParry -= OnCreatureAttackParry;
+        EventManager.onCreatureAttackParry += OnCreatureAttackParry;
+    }
+    
+    public override void OnItemUnimbued(Item item, Imbue imbue, SpellCastCharge spellCastCharge)
+    {
+        base.OnItemUnimbued(item, imbue, spellCastCharge);
+        EventManager.onCreatureAttackParry -= OnCreatureAttackParry;
+    }
+
+    private void OnCreatureAttackParry(Creature parriedcreature, Item parrieditem, Creature parryingcreature, Item parryingitem, CollisionInstance hit)
+    {
+        bool hasImbue = parryingitem.imbues.Find(x => x.spellCastBase is SpellCastGravity) != null;
+        if (!hasImbue || EnrichmentManager.HasEnrichment(parryingitem, id) || !IsValidTarget(parriedcreature, null) || hit.impactVelocity.sqrMagnitude < requiredVelocity * requiredVelocity) return;
+        
+        Vector3 direction = parriedcreature.ragdoll.targetPart.transform.position - hit.contactPoint;
+        parriedcreature.TryPush(Creature.PushType.Magic, direction, 1, RagdollPart.Type.Torso);
+        parriedcreature.AddForce(direction * forceMultiplier, ForceMode.Impulse);
+        effectData?.Spawn(hit.contactPoint, Quaternion.LookRotation(direction)).Play();
+        seenCreatures.Add(parriedcreature);
+        parriedcreature.StartCoroutine(CooldownRoutine(parriedcreature));
+    }
+    
+    private IEnumerator CooldownRoutine(Creature target)
+    {
+        yield return Yielders.ForRealSeconds(creatureCooldown);
+        seenCreatures.Remove(target);
+    }
+}
---
+++ EnrichmentMassDriver.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using ThunderRoad;
+using ThunderRoad.Skill.Spell;
+using UnityEngine;
+
+namespace Enrichments;
+
+public class EnrichmentMassDriver : EnrichmentData
+{
+    public string effectId;
+    public float searchRadius = 3f;
+    public float pointOffset = 1.5f;
+    public float forceMultiplier = 15f;
+    public float requiredVelocity = 0.5f;
+    public float creatureCooldown = 2f;
+    
+    public EffectData effectData;
+
+    private List<Creature> seenCreatures = new();
+    
+    public bool IsValidTarget(Creature target, SpellCastCharge spellData) => target != null && !target.isCulled && target != spellData?.spellCaster.ragdollHand.creature && !seenCreatures.Contains(target);
+
+    public override void OnCatalogRefresh()
+    {
+        base.OnCatalogRefresh();
+        effectData = Catalog.GetData<EffectData>(effectId);
+    }
+
+    public override void OnItemImbued(Item item, Imbue imbue, SpellCastCharge spellCastCharge)
+    {
+        base.OnItemImbued(item, imbue, spellCastCharge);
+        if (spellCastCharge is not SpellCastGravity) return;
+        imbue.OnImbueHit -= OnImbueHit;
+        imbue.OnImbueHit += OnImbueHit;
+    }
+
+    public override void OnItemUnimbued(Item item, Imbue imbue, SpellCastCharge spellCastCharge)
+    {
+        base.OnItemUnimbued(item, imbue, spellCastCharge);
+        imbue.OnImbueHit -= OnImbueHit;
+    }
+
+    private void OnImbueHit(SpellCastCharge spelldata, float amount, bool fired, CollisionInstance hit, EventTime eventtime)
+    {
+        Creature hitCreature = hit?.targetColliderGroup?.collisionHandler?.ragdollPart?.ragdoll?.creature;
+        if (hit == null || hitCreature is null || spelldata is not SpellCastGravity || fired || eventtime != EventTime.OnStart || !IsValidTarget(hitCreature, spelldata) || hit.impactVelocity.sqrMagnitude < requiredVelocity * requiredVelocity) return;
+        
+        foreach (Creature creature in Creature.InRadius(hitCreature.ragdoll.targetPart.transform.position, searchRadius, creature => IsValidTarget(creature, spelldata)))
+        {
+            Vector3 start = creature.ragdoll.targetPart.transform.position - hit.impactVelocity.normalized * pointOffset;
+            Vector3 direction = hit.impactVelocity.normalized;
+            creature.TryPush(Creature.PushType.Magic, direction, 1, RagdollPart.Type.Torso);
+            creature.AddForce(direction * forceMultiplier, ForceMode.Impulse);
+            effectData?.Spawn(start, Quaternion.LookRotation(direction)).Play();
+            seenCreatures.Add(creature);
+            creature.StartCoroutine(CooldownRoutine(creature));
+        }
+    }
+    
+    private IEnumerator CooldownRoutine(Creature target)
+    {
+        yield return Yielders.ForRealSeconds(creatureCooldown);
+        seenCreatures.Remove(target);
+    }
+}
